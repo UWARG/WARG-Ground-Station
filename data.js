c0e3b3e43bf9bb6028397c366ec9a1d73257ab16
zhaop <zhaopatrick@gmail.com>
2015-02-03 14:58:59 -0500

    Add comment to request documentation for the data headers

--- data.js
+++ data.js
@@ -5,10 +5,26 @@ var Data = {
 };
 
 /*
-headers:
-	lat, lon, time, pitch, roll, yaw, pitch_rate, roll_rate, yaw_rate,
-	kd_gain, kp_gain, ki_gain, ground_speed, altitude, heading,
-	pitch_setpoint, roll_setpoint, heading_setpoint, throttle_setpoint,
-	altitude_setpoint, int_pitch_setpoint, int_roll_setpoint, int_yaw_setpoint,
-	lastCommandSent, errorCodes, cameraStatus, waypointIndex, editing_gain, gpsStatus
+headers (in order):
+	lat, lon,			// degrees
+	time,				// hhmmss (float, decimals are fractions of a second)
+	pitch, roll, yaw,	// rad ?
+	pitch_rate, roll_rate, yaw_rate,	// rad/s ?
+	kd_gain, kp_gain, ki_gain,			// ?
+	ground_speed,		// m/s ?
+	altitude,			// m ?
+	heading,			// degrees
+	pitch_setpoint, roll_setpoint, heading_setpoint,			// degrees ?
+	throttle_setpoint,	// ?
+	altitude_setpoint,	// m ?
+	int_pitch_setpoint, int_roll_setpoint, int_yaw_setpoint,	// ?
+	lastCommandSent,	// ?
+	errorCodes,			// ?
+	cameraStatus,		// ?
+	waypointIndex,		// Index of current waypoint ?
+	editing_gain,		// ?
+	gpsStatus			// ?
+
+TODO: Could someone check and make sure the units are right?
+And explain some of the magic status numbers like lastCommandSent, errorCodes, cameraStatus, gpsStatus etc?
 */
