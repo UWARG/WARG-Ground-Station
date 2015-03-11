656915b2fada6182867550959d9e6a7ca650bee2
Patrick Zhao <zhaopatrick@gmail.com>
2015-03-11 03:58:17 -0400

    Add F11 shortcut to toggle fullscreen (& Esc to exit)

--- nw-shortcuts.js
+++ nw-shortcuts.js
@@ -4,4 +4,6 @@
 	Mousetrap.bind(["mod+r"], function() { win.reloadDev() });
 	Mousetrap.bind(["mod+shift+r", "shift+f5"], function() { win.reloadIgnoringCache() });
 	Mousetrap.bind(["mod+shift+i", "mod+shift+j"], function() { win.isDevToolsOpen() ? win.closeDevTools() : win.showDevTools() });
+	Mousetrap.bind(["f11"], function() { win.toggleFullscreen() });
+	Mousetrap.bind(["esc"], function() { win.isFullscreen && win.toggleFullscreen() })
 })(Mousetrap);
