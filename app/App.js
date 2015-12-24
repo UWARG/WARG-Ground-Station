8f9cdd6e7e100b19fd770372c9e02978a02136be
Serge Babayan <serj.b.babayan@gmail.com>
2015-12-24 16:06:08 -0500

    Got a view working

--- app/App.js
+++ app/App.js
@@ -1,19 +1,24 @@
-// Load native UI library
 var gui = require('nw.gui');
 var app_config=require('./config/application-config');
 var Logger=require('./app/util/Logger');
 
-if(app_config.mode==='development'){
-  //require('./app/livereload').start();
-  Logger.debug('Live reload server started!');
-}
-
 var app={};
 
 app.menu=require('./app/Menu')(gui);
+app.windows=[]; //an array of all the windows open
 
 // Append Menu to Window
 gui.Window.get().menu = app.menu;
-gui.Window.get().on('focus',function(){
-  console.log('focusing');
+
+
+var MainLayoutView=require('./app/MainLayoutView')(Marionette,_);
+
+$(document).ready(function(){
+  $('body').append((new MainLayoutView()).render().$el);
 });
+
+//Start the live reload server if in development mode
+if(app_config.mode==='development'){
+  require('./app/util/livereload').start();
+  Logger.debug('Live reload server started!');
+}
---
+++ app/MainLayoutView.js
@@ -0,0 +1,11 @@
+module.exports=function(Marionette,_){
+
+	var MainLayoutView=Marionette.LayoutView.extend({
+		template:_.template("<div>hello!ewfwef</div>"),
+		initialize: function(){
+
+		}
+	});
+
+	return MainLayoutView;
+}
