fb544a97a7385ed2330746079c5b0d9c750c1bdb
Serge Babayan <serj.b.babayan@gmail.com>
2016-01-01 22:07:52 -0500

    Refactored leaflet draw into its own module

--- app/Map.js
+++ app/Map.js
@@ -1,6 +1,7 @@
 //a wrapper around leaflet that configures it as well
 var map_config=require('../config/map-config');
 var path=require('path');
+var MapDraw=require('./models/MapDraw');
 
 var Map=function(L){
   var leaflet=L;//reference to the window leaflet object
@@ -60,51 +61,6 @@ var Map=function(L){
     }
   });
   
-  // Initialise the FeatureGroup to store editable layers
-  var drawnItems = new leaflet.FeatureGroup();
-
-  var leaflet_draw_options = {
-    position: 'topleft',
-    draw: {
-      polyline: {
-        shapeOptions: {
-          color: '#8774FF',
-          weight: 4,
-          //fillColor:''
-        }
-      },
-      polygon: {
-        shapeOptions: {
-          color: '#00B0CA',
-          weight: 4,
-          //fillColor:''
-        }
-      },
-      circle: {
-        shapeOptions: {
-          color: '#FC2F2F',
-          weight: 4,
-          //fillColor:''
-        }
-      },
-      rectangle: {
-        shapeOptions: {
-          color: '#229C00',
-          weight: 4,
-          //fillColor:''
-        }
-      },
-      marker: {
-        
-      }
-    },
-    edit: {
-      featureGroup: drawnItems //Important!
-    }
-  };
-  // Initialise the draw control and pass it the FeatureGroup of editable layers
-  var drawControl = new leaflet.Control.Draw(leaflet_draw_options);
-
   this.createMap=function(id){
     map = leaflet.map(id,{
       center: map_config.default_lat_lang,
@@ -118,15 +74,7 @@ var Map=function(L){
     leaflet.control.mousePosition().addTo(map);
     map.addControl(centerToPlaneButton);
     map.addControl(measureControl);
-    drawnItems.addTo(map);
-
-    map.addControl(drawControl);
-
-    map.on('draw:created', function(event) {
-      var layer = event.layer;
-      drawnItems.addLayer(layer);
-    });
-
+    new MapDraw(leaflet).addTo(map); //adds draw controls to map
   };
 
   this.movePlane=function(lat,lng, heading){
