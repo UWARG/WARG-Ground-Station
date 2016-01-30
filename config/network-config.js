11bd210c88d783ec9849574a2e82feca9e566c8b
Serge Babayan <serj.b.babayan@gmail.com>
2016-01-29 19:58:29 -0500

    Temp commit

--- config/network-config.js
+++ config/network-config.js
@@ -1,3 +1,5 @@
+var SettingsLoader=require('../app/util/SettingsLoader');
+
 var network_config={
 	datarelay_host:'127.0.0.1',
 	datarelay_port:'1234',
@@ -7,4 +9,4 @@ var network_config={
   multiecho:'0' //in milliseconds, the amount of time a socket will wait idle before declaring itself as timed out and closing the connection
 };
 
-module.exports=network_config;
+module.exports=new SettingsLoader('network_config',network_config);
