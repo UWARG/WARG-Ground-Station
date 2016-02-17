e168b677c31358953276a3cd7b9edb50f259e41a
Serge Babayan <serj.b.babayan@gmail.com>
2016-02-17 13:19:56 -0500

    Space and tab fixes

--- app/views/AutoAdjustView.js
+++ app/views/AutoAdjustView.js
@@ -2,97 +2,84 @@ var Template = require('../util/Template');
 var Logger = require('../util/Logger');
 var Commands = require('../models/Commands');
 
-module.exports = function (Marionette) {
+module.exports = function(Marionette) {
 
-    return Marionette.ItemView.extend({
-        template: Template('AutoAdjustView'),
-        className: 'gainsAdjustView', 
+  return Marionette.ItemView.extend({
+    template: Template('AutoAdjustView'),
+    className: 'gainsAdjustView',
 
-        ui: { 
+    ui: {
+      all_button: "#sendall",
+      rc_button: "#fullRC",
+      auto_button: "fullAuto",
+      ground_button: "fullGround",
+      pitch_select: "#pitchsource",
+      roll_select: "#rollsource",
+      head_select: "#headsource",
+      alt_select: "#altsource",
+      throttle_select: "#throttlesource",
+      flap_select: "#flapssource",
+      rolltype_select: "#rolltype",
+      pitchtype_select: "#pitchtype"
+    },
 
-            all_button: "#sendall",
-            rc_button: "#fullRC",
-            auto_button: "fullAuto",
-            ground_button: "fullGround",
-            pitch_select: "#pitchsource",
-            roll_select: "#rollsource",
-            head_select: "#headsource",
-            alt_select: "#altsource",
-            throttle_select: "#throttlesource",
-            flap_select: "#flapssource",
-            rolltype_select: "#rolltype",
-            pitchtype_select: "#pitchtype"
+    events: {
+      "click #sendall": "sendall",
+      "click #fullRC": "fullRC",
+      "click #fullAuto": "fullAuto",
+      "click #fullGround": "fullGround"
+    },
 
-        },
-     
-        events: {
-            "click #sendall": "sendall",
-            "click #fullRC": "fullRC",
-            "click #fullAuto":"fullAuto",
-            "click #fullGround":"fullGround"
-        },
-        
-        sendall: function (event) { 
-            var autolevel=0;
-           
-            if (this.ui.head_select.val()==="Off")
-            {
-                
-            } else
-            {
-                autolevel = autolevel + Math.pow(2,9);
-            }
-            if (this.ui.head_select.val()==="Autopilot")
-            {
-                autolevel = autolevel + Math.pow(2,8);
-            } 
-            if (this.ui.alt_select.val()==="Off")
-            {
-                
-            } else
-            {
-                autolevel = autolevel + Math.pow(2,7);
-            }
-            if (this.ui.alt_select.val()==="Autopilot")
-            {
-                autolevel = autolevel + Math.pow(2,6);
-            } 
-            
-             if(this.ui.throttle_select.val()==="Ground Station")
-            {
-                autolevel=autolevel+ Math.pow(2,4);
-            }
-            else if(this.ui.throttle_select.val()==="Autopilot")
-            {
-                autolevel=autolevel+Math.pow(2,5);
-            }
-            if (this.ui.roll_select.val()==="Ground Station")
-            {
-                autolevel = autolevel + Math.pow(2,3);
-            } 
-            if (this.ui.rolltype_select.val()==="Angle")
-            {
-               autolevel = autolevel + Math.pow(2,2);
-            } 
-            if (this.ui.pitch_select.val()==="Ground Station")
-            {
-                autolevel = autolevel + Math.pow(2,1);
-            } 
-            if (this.ui.pitchtype_select.val()==="Angle")
-            {
-               autolevel = autolevel +1; 
-            } 
-           
-            Commands.sendAutoLevel(autolevel);
-        },
-        fullRC: function (event) { 
-            Commands.sendAutoLevel(0);
-        },
-        fullAuto: function (event) { //full autopilot and groundstation (defaults to angle)
-            Commands.sendAutoLevel(1007);
-        },
-        fullGround: function (event) { //full groundstation (defaults to angle)
-            Commands.sendAutoLevel(671);
-        }
-    });
+    sendall: function(event) {
+      var autolevel = 0;
+
+      if (this.ui.head_select.val() === "Off") {
+
+      } else {
+        autolevel = autolevel + Math.pow(2, 9);
+      }
+      if (this.ui.head_select.val() === "Autopilot") {
+        autolevel = autolevel + Math.pow(2, 8);
+      }
+      if (this.ui.alt_select.val() === "Off") {
+
+      } else {
+        autolevel = autolevel + Math.pow(2, 7);
+      }
+      if (this.ui.alt_select.val() === "Autopilot") {
+        autolevel = autolevel + Math.pow(2, 6);
+      }
+
+      if (this.ui.throttle_select.val() === "Ground Station") {
+        autolevel = autolevel + Math.pow(2, 4);
+      } else if (this.ui.throttle_select.val() === "Autopilot") {
+        autolevel = autolevel + Math.pow(2, 5);
+      }
+      if (this.ui.roll_select.val() === "Ground Station") {
+        autolevel = autolevel + Math.pow(2, 3);
+      }
+      if (this.ui.rolltype_select.val() === "Angle") {
+        autolevel = autolevel + Math.pow(2, 2);
+      }
+      if (this.ui.pitch_select.val() === "Ground Station") {
+        autolevel = autolevel + Math.pow(2, 1);
+      }
+      if (this.ui.pitchtype_select.val() === "Angle") {
+        autolevel = autolevel + 1;
+      }
+      Commands.sendAutoLevel(autolevel);
+    },
+
+    fullRC: function(event) {
+      Commands.sendAutoLevel(0);
+    },
+
+    fullAuto: function(event) { //full autopilot and groundstation (defaults to angle)
+      Commands.sendAutoLevel(1007);
+    },
+    
+    fullGround: function(event) { //full groundstation (defaults to angle)
+      Commands.sendAutoLevel(671);
+    }
+  });
 };
--- app/views/DataEntryView.js
+++ app/views/DataEntryView.js
@@ -1,40 +1,35 @@
+var fs = require('fs');
 
 var Template = require('../util/Template');
 var TelemetryData = require('../models/TelemetryData')
-var fs = require('fs');
 var Logger = require('../util/Logger');
-var app_config=require('./config/application_config.js');
+var app_config = require('./config/application_config.js');
 
+module.exports = function(Marionette) {
 
-module.exports = function (Marionette) {
+  return Marionette.ItemView.extend({
+    template: Template('DataEntryView'),
+    className: 'gainsAdjustView',
 
-    return Marionette.ItemView.extend({
-        template: Template('DataEntryView'), 
-        className: 'gainsAdjustView', 
+    ui: {
+      name_field: "#fileName",
+      comment_field: "#comment",
+      save_button: "#saveComment"
+    },
 
-        ui: {
-            name_field: "#fileName",
-            comment_field: "#comment",
-            save_button: "#saveComment"
-     
-        },
-      
-        events: {
-            "click #saveComment": "saveData",
-            
-        },
-        
-        saveData: function (event) { 
-            var d=new Date();
-            var loc = "./"+app_config.get('log_dir') +"/data_entries/" + this.ui.name_field.val()+"_"+d.getDate()+".txt";
-            var content="Comment: " +this.ui.comment_field.val() + "\r\nData Stream: " +JSON.stringify(TelemetryData.current_state);
-           
-             fs.writeFile(loc, content, 'utf8', function(err){
-                 
-             });
-             Logger.debug("file "+this.ui.name_field.val()+" written to "+loc);
-        }
-  
+    events: {
+      "click #saveComment": "saveData",
+    },
+
+    saveData: function(event) {
+      var d = new Date();
+      var loc = "./" + app_config.get('log_dir') + "/data_entries/" + this.ui.name_field.val() + "_" + d.getDate() + ".txt";
+      var content = "Comment: " + this.ui.comment_field.val() + "\r\nData Stream: " + JSON.stringify(TelemetryData.current_state);
+
+      fs.writeFile(loc, content, 'utf8', function(err) {
+
+      });
+      Logger.debug("file " + this.ui.name_field.val() + " written to " + loc);
     }
-);
+  });
 };
