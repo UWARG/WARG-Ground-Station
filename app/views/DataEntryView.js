85801f3587c3e5b2cbefdfd71b880c89a7651565
Serge Babayan <serj.b.babayan@gmail.com>
2016-02-17 13:30:59 -0500

    Using mkdirp to create data entry directory if it doesnt exist yet

--- app/views/DataEntryView.js
+++ app/views/DataEntryView.js
@@ -1,9 +1,16 @@
 var fs = require('fs');
+var mkdirp=require('mkdirp');
+var path=require('path');
 
 var Template = require('../util/Template');
 var TelemetryData = require('../models/TelemetryData')
 var Logger = require('../util/Logger');
-var app_config = require('./config/application_config.js');
+var app_config = require('../../config/application-config');
+
+var write_location=path.join(__dirname,'../../'+app_config.get('log_dir') + "data_entries/");
+
+//create the data entry directory if it doesnt yet exist
+mkdirp.sync(write_location);
 
 module.exports = function(Marionette) {
 
@@ -23,13 +30,17 @@ module.exports = function(Marionette) {
 
     saveData: function(event) {
       var d = new Date();
-      var loc = "./" + app_config.get('log_dir') + "/data_entries/" + this.ui.name_field.val() + "_" + d.getDate() + ".txt";
+      var loc = write_location + this.ui.name_field.val() + "_" + d.getDate() + ".txt";
       var content = "Comment: " + this.ui.comment_field.val() + "\r\nData Stream: " + JSON.stringify(TelemetryData.current_state);
 
       fs.writeFile(loc, content, 'utf8', function(err) {
-
-      });
-      Logger.debug("file " + this.ui.name_field.val() + " written to " + loc);
+        if(err){
+          Logger.error('Error writing data entry to file: '+err);
+        }
+        else{
+          Logger.debug("file " + this.ui.name_field.val() + " written to " + loc);
+        }
+      }); 
     }
   });
 };
