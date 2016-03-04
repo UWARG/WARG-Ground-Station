3a71531f65a568113b93b34786c5753f0f8369e9
trevormccrt <trevor.mccourt@uwaterloo.ca>
2016-03-04 18:38:33 -0500

    fixes

--- app/util/GainsManip.js
+++ app/util/GainsManip.js
@@ -3,34 +3,42 @@ var fdialogs = require('node-webkit-fdialogs');
 var Logger = require('../util/Logger');
 var Commands = require('../models/Commands');
 var gains_config = require('../../config/gains-config');
-//Loads in configuration from config files into local storage if they dont exist. Subsequent
 var GainsManip = {
     GainsExport: function () {
-        var keys=Object.keys(gains_config.default_settings);
+        var keys = Object.keys(gains_config.default_settings);
         var str;
         var buf;
-        var exported_settings=gains_config.default_settings;
+        var exported_settings = gains_config.default_settings;
         for (var i = 0; i < keys.length; i++)
         {
-            exported_settings[keys[i]]=gains_config.get(keys[i]);
+            exported_settings[keys[i]] = gains_config.get(keys[i]);
         }
-        str=JSON.stringify(exported_settings,null,2);
-        buf=new Buffer(str);
+        str = JSON.stringify(exported_settings, null, 2);
+        buf = new Buffer(str);
         fdialogs.saveFile(buf, function (err, path) {
+            Logger.debug("file saved succesfully to " + path);
         });
     },
     GainsImport: function () {
         var Dialog = new fdialogs.FDialog({
             type: 'open',
             accept: ['.txt'],
-            path: '~Documents'
+            path: '~/Documents'
         });
         fdialogs.readFile(function (err, content, path) {
-            var keys=Object.keys(gains_config.default_settings);
-            var object=JSON.parse(content,null,2);
-            for(var i=0;i<keys.length;i++)
+            var keys = Object.keys(gains_config.default_settings);
+            try {
+                var object = JSON.parse(content, 2);
+            } catch (e)
             {
-                gains_config.set(keys[i],object[keys[i]]);
+                Logger.info("could not save, bad file format" + e);
+            }
+            for (var i = 0; i < keys.length; i++)
+            {
+                if (keys[i] in object)
+                {
+                    gains_config.set(keys[i], object[keys[i]]);
+                }
             }
         });
     }
