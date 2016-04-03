4455c679171a8d71745602b341192ddd82688d7b
Serge Babayan <serj.b.babayan@gmail.com>
2016-04-03 13:21:58 -0400

    Added ability to change waypoint altitude and number

--- app/lib/leaflet-plugins/custom/leaflet.waypoint.js
+++ app/lib/leaflet-plugins/custom/leaflet.waypoint.js
@@ -2,22 +2,47 @@
 //This file contains the waypoint class used within the MapView
 
 var waypointIcon = L.divIcon({className: 'waypointIcon',html:'<div class="inner-circle"><p>#1</p><p>100m</p></div>'});
+var counter=0;
 
 L.waypoint= L.Marker.extend({
 	initialize: function (latlngalt, options) { //latlngalt is an array of size 3 in this manner: [lat, lon, alt]
-      options=options || {};
-      options.icon=waypointIcon; //give it the proper icon
-      options.draggable=true;
-      L.Marker.prototype.initialize.call(this, latlngalt, options); //i dont think this is necessary
-    	// if (!(L.Util.isArray(latlngalt) && latlngalt.length === 3)) {
-    	// 	throw new Error("L.AltitudeMarker must be initialized with an array latitude, longitude, and altitude [lat,lon,alt]");
-    	// }
-
-     //  else if (isNaN(latlngalt[0]) || isNaN(latlngalt[1]) || isNaN(latlngalt[2]) || Number(latlngalt[2])<=0){
-     //     throw new Error("L.AltitudeMarker must be initialized with a valid longitude, latitude, and altitude values and have a positive altitude value"); 
-     //  }
-     //  else{
-        
-     //  }
+    if(L.Util.isArray(latlngalt)){
+      if(latlngalt.length!==3){
+        throw new Error('L.waypoint must be initialized with an array of 3 [lat, lon, alt] or an object');
+      }
     }
+    else{
+      if(!(latlngalt && latlngalt.alt && latlngalt.lat && latlngalt.lng)){
+        throw new Error('L.waypoint must be with an object that contains lat,lng, and alt properties');
+      }
+    }
+    options=options || {};
+    options.icon=waypointIcon; //give it the proper icon
+    options.draggable=true;
+    options.waypointCount= options.waypointCount || 0;
+    this.waypointCount=options.waypointCount;
+    L.Marker.prototype.initialize.call(this, latlngalt, options);
+    this.updateIcon();
+    this.changeWaypointCount(6)
+  },
+
+  changeAltitude: function(new_alt){
+    if(!isNaN(new_alt)){
+      this._latlng.alt=new_alt;
+      this.updateIcon();
+    }else{
+      console.error('L.waypoint.changeAltitude takes in a number parameter. Received: '+new_alt);
+    }
+  },
+
+  changeWaypointCount: function(new_number){
+    if(!isNaN(new_number)){
+      this.waypointCount=new_number;
+      this.updateIcon();
+    }
+  },
+
+  updateIcon: function(){
+    this.options.icon.options.html='<div class="inner-circle"><p>#'+this.waypointCount+'</p><p>'+this._latlng.alt+'m</p></div>';
+  }
 });
