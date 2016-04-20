839fc858b016ac91d89785e7c35090c41eafc703
Serge Babayan <serj.b.babayan@gmail.com>
2016-04-20 17:51:43 -0400

    Improved performance of the groundstation by adding a limit on the max number of messages in the console

--- config/advanced-config.js
+++ config/advanced-config.js
@@ -3,7 +3,8 @@ var SettingsLoader=require('../app/util/SettingsLoader');
 var advanced_config={
   logger_max_listeners:20, //maximum number of listeners that can attach to the Logger object (should increase if you plan on having lots of consoles open)
   connection_max_listeners:40, //maximum number of listeners that can attach to the Connection object (should increase if lots of things need telemetry data and what not)
-  telemetrydata_max_listeners: 40
+  telemetrydata_max_listeners: 40,
+  max_console_messages: 100
 };
 
 module.exports=new SettingsLoader('advanced_config',advanced_config);
