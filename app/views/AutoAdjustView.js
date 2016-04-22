15df8fa065a34e5ae8e739a626ed8db0ffc82729
Serj Babayan <serj.b.babayan@gmail.com>
2016-04-22 18:35:35 -0400

    Displaying remote autonomous state

--- app/views/AutoAdjustView.js
+++ app/views/AutoAdjustView.js
@@ -24,7 +24,21 @@ module.exports = function(Marionette) {
       rolltype_select: "#rolltype",
       pitchtype_select: "#pitchtype",
       alttype_select: '#altitudetype',
-      headingtype_select: '#headingtype'
+      headingtype_select: '#headingtype',
+
+      remote_pitch_type:'#remote_pitch_type',
+      remote_roll_type:'#remote_roll_type',
+      remote_heading_toggle:'#remote_heading_toggle',
+      remote_altitude_toggle:'#remote_altitude_toggle',
+      remote_pitch_source:'#remote_pitch_source',
+      remote_roll_source:'#remote_roll_source',
+      remote_heading_source:'#remote_heading_source',
+      remote_altitude_source:'#remote_altitude_source',
+      remote_throttle_source:'#remote_throttle_source',
+      remote_flap_source:'#remote_flap_source',
+
+      remote_autonomous_level_value: '#remote_autonomous_level_value',
+      autonomous_level_synced:'#autonomous_level_synced'
     },
 
     events: {
@@ -38,12 +52,99 @@ module.exports = function(Marionette) {
       this.telemetryCallback=null;
     },
 
+    onRender: function(){
+      TelemetryData.on('data_received', this.dataReceivedCallback.bind(this));
+    },
+/*
+0000000000b = Full manual control (default)
+0000000001b = Set Pitch Rate(0), Pitch Angle(1)
+0000000010b = Pitch Control Source: Controller(0), Ground Station(1) 
+0000000100b = Roll Control Type: Roll Rate(0), Roll Angle(1)
+0000001000b = Roll Control Sources: Controller(0), Ground Station(1)
+0000110000b = Throttle control source: Controller(0), Ground Station(1), Autopilot(2)
+0001000000b = Altitude Source: Ground Station(0), Autopilot(1)
+0010000000b = Altitude Control On(1) or Off(0)
+0100000000b = Heading control source: Ground Station(0), Autopilot(1)
+1000000000b= To fly with Ground Station Control of the Pitch Rate and Roll Angle:
+*/
     dataReceivedCallback: function(data){
-      var picpilot_autonomous_level=new Bitmask(Number(data.automous_level));
-      
+      this.ui.remote_autonomous_level_value.text(data.autonomousLevel);
+      this.ui.autonomous_level_synced.text(Number(data.autonomousLevel)===this.encodeInputs() ? 'Yes' : 'No');
+      var alevel=new Bitmask(Number(data.autonomousLevel));
+
+      if(alevel.getBit(0)){
+        this.ui.remote_pitch_type.text('Angle');
+      }
+      else{
+        this.ui.remote_pitch_type.text('Rate');
+      }
+      if(alevel.getBit(1)){
+        this.ui.remote_pitch_source.text('Ground Station');
+      }
+      else{
+        this.ui.remote_pitch_source.text('Controller');
+      }
+      if(alevel.getBit(2)){
+        this.ui.remote_roll_type.text('Angle');
+      }
+      else{
+        this.ui.remote_roll_type.text('Rate');
+      }
+      if(alevel.getBit(3)){
+        this.ui.remote_roll_source.text('Ground Station');
+      }
+      else{
+        this.ui.remote_roll_source.text('Controller');
+      }
+      if(alevel.getBit(4) && alevel.getBit(5)){
+        this.ui.remote_throttle_source.text('Autopilot');
+      }
+      else if (alevel.getBit(4)){
+        this.ui.remote_throttle_source.text('Groundstation'); 
+      }
+      else{
+        this.ui.remote_throttle_source.text('Controller');
+      }
+      if(alevel.getBit(6)){
+        this.ui.remote_altitude_source.text('Autopilot');
+      }
+      else{
+        this.ui.remote_altitude_source.text('Groundstation');
+      }
+      if(alevel.getBit(7)){
+        this.ui.remote_altitude_toggle.text('On');
+      }
+      else{
+        this.ui.remote_altitude_toggle.text('Off');
+      }
+      if(alevel.getBit(8)){
+        this.ui.remote_heading_source.text('Autopilot');
+      }
+      else{
+        this.ui.remote_heading_source.text('Groundstation');
+      }
+      if(alevel.getBit(9)){
+        this.ui.remote_heading_toggle.text('On');
+      }
+      else{
+        this.ui.remote_heading_toggle.text('Off');
+      }
+      if(alevel.getBit(10) && alevel.getBit(11)){
+        this.ui.remote_flap_source.text('Autopilot');
+      }
+      else if(alevel.getBit(10)){
+        this.ui.remote_flap_source.text('Groundstation');
+      }
+      else{
+        this.ui.remote_flap_source.text('Controller');
+      }
     },
 
     sendAll: function(event) {
+      Commands.sendAutoLevel(this.encodeInputs());
+    },
+
+    encodeInputs: function(){
       var autolevel = 0;
       if (this.ui.flap_select.val() === "Autopilot") {
         autolevel = autolevel + Math.pow(2, 11);
@@ -81,7 +182,7 @@ module.exports = function(Marionette) {
       if (this.ui.pitchtype_select.val() === "Angle") {
         autolevel = autolevel + 1;
       }
-      Commands.sendAutoLevel(autolevel);
+      return autolevel;
     },
 
     fullRC: function(event) {
