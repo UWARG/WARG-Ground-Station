66a6567809b5e44acdb0a332f44701e1f7aba8dc
Serj Babayan <serj.b.babayan@gmail.com>
2016-06-07 22:47:43 -0400

    Potentially working data relay implementation

--- app/connections/DataRelay.js
+++ app/connections/DataRelay.js
@@ -1,14 +1,146 @@
-//This file configures the datarelay connection and parses its data
+/**
+ * @author Serge Babayan
+ * @module connections/DataRelay
+ * @requires config/network-config
+ * @requires config/map-config
+ * @requires Network
+ * @requires util/Logger
+ * @requires StatusManger
+ * @requires models/Commands
+ * @requires models/TelemetryData
+ * @copyright Waterloo Aerial Robotics Group 2016
+ * @licence https://raw.githubusercontent.com/UWARG/WARG-Ground-Station/master/LICENSE
+ * @description Configures the data relay connection for connecting to it, and parsing its data and sending it out
+ * to the TelemetryData module
+ * @extends EventEmitter
+ * @see http://docs.uwarg.com/picpilot/datalink/
+ */
 var network_config = require('../../config/network-config');
 var Network = require('../Network');
 var Logger = require('../util/Logger');
 var TelemetryData = require('../models/TelemetryData');
 var StatusManager = require('../StatusManager');
 var Commands = require('../models/Commands');
-var map_config = require('../../config/map-config');
+var PacketTypes = require('../models/PacketTypes');
 
-module.exports.init = function () {
-  if (Network.connections['data_relay']) { //if a connection has already been established (occurs on a page refresh), destroy it
+var DataRelay = {};
+
+DataRelay.checkForMissingHeaders = function (data) {
+  var expected_headers = {};
+  var header;
+
+  //create a list of the headers we expect to receive and set their value to 0
+  for (var packet_type_name in PacketTypes) {
+    if (PacketTypes.hasOwnProperty(packet_type_name)) {
+      for (header in PacketTypes[packet_type_name]) {
+        if (PacketTypes[packet_type_name].hasOwnProperty(header)) {
+          expected_headers[header] = 0;
+        }
+      }
+    }
+  }
+
+  //iterate through the data packet that we actually received
+  for (var i = 0; i < TelemetryData.headers.length; i++) {
+    var header_name = TelemetryData.headers[i];
+
+    //if we receive a header that wasn't listed in the PacketTypes model
+    if (expected_headers[header_name] === undefined) {
+      Logger.warn('An unexpected header was received from the data relay. Header: ' + header_name);
+    }
+    //if we received the same header more than once
+    else if (expected_headers[header_name] > 0) {
+      expected_headers[header_name]++;
+      Logger.warn('Header: ' + header_name + ' was received more than once! Times: ' + expected_headers[header_name]);
+    }
+    else if (expected_headers[header_name] == 0) {
+      expected_headers[header_name]++;
+    }
+  }
+
+  //check to see if all the expected headers were received
+  for (header in expected_headers) {
+    if (expected_headers.hasOwnProperty(header) && expected_headers[header] === 0) {
+      Logger.warn('Did not receive an expected header! Header: ' + header);
+    }
+  }
+};
+
+DataRelay.parseHeaders = function (data) {
+  TelemetryData.headers = data.split(",").map(function (str) {
+    return str.trim();
+  });
+
+  Logger.debug('Network ' + this.name + ' Received headers: ' + data);
+  Logger.debug('Network ' + this.name + ' Parsed headers: ' + JSON.stringify(TelemetryData.headers));
+  Logger.data(TelemetryData.headers, 'DATA_RELAY_HEADERS');
+  StatusManager.addStatus('Received headers from data_relay', 3, 3000);
+};
+
+DataRelay.parseData = function (data) {
+  var split_data = data.split(",");
+
+  //warn the user if we receive an improper number of data
+  if (split_data.length !== TelemetryData.headers.length) {
+    Logger.error('Number of data headers doesn\'t match the number of data!. Length of data: ' + split_data.length + ' Length of headers: ' + TelemetryData.headers.length);
+  }
+
+  for (var i = 0; i < split_data.length; i++) {
+    //the replace is required because there's a chance of random brackets being in the values (with past logs at least)
+    TelemetryData.current_state[TelemetryData.headers[i]] = split_data[i];
+  }
+  TelemetryData.state_history.push(TelemetryData.current_state);
+  TelemetryData.emit('data_received', TelemetryData.current_state);
+
+  for (var packet_type_name in PacketTypes) {
+    if (PacketTypes.hasOwnProperty(packet_type_name)) {
+      var data = {};
+      for (var header in PacketTypes[packet_type_name]) {
+        if (PacketTypes[packet_type_name].hasOwnProperty(header)) {
+          if (TelemetryData.current_state[header] === null) {
+            data[header] = null;
+          }
+          else {
+            var validation_functions = PacketTypes[packet_type_name][header];
+            if (typeof validation_functions === 'string') { //if its only a single validation function
+              if (Validator[validation_functions](TelemetryData.current_state[header])) {
+                data[header] = Number(TelemetryData.current_state[header]);
+              }
+              else {
+                Logger.warn('Invalid data received for ' + header + '. Value: ' + TelemetryData.current_state[header]);
+                data[header] = null;
+              }
+            }
+            else{ //if its in array and there are multiple validation functions
+              var failed = false;
+              for(var i = 0;i< validation_functions.length; i++){
+                if(!validation_functions[i](TelemetryData.current_state[header])){
+                  failed = true;
+                }
+              }
+              if(failed){
+                data[header] = null;
+              }else{
+                data[header] = Number(TelemetryData.current_state[header]);
+              }
+            }
+          }
+        }
+      }
+      TelemetryData.emit(packet_type_name, data);
+    }
+  }
+
+  Logger.data(JSON.stringify(TelemetryData.current_state), 'DATA_RELAY_DATA');
+  StatusManager.setStatusCode('TIMEOUT_DATA_RELAY', false);
+};
+
+/**
+ * Initializes or re-initializes the data relay connection and sets up callbacks for parsing any received data
+ * @function init
+ */
+DataRelay.init = function () {
+  if (Network.connections['data_relay']) { //if a connection has already been established, disconnect it
     Network.connections['data_relay'].disconnect();
   }
 
@@ -18,16 +150,10 @@ module.exports.init = function () {
 
   data_relay.on('connect', function () {
     StatusManager.setStatusCode('CONNECTED_DATA_RELAY', true);
-
-    setTimeout(function () {
-      Commands.activateWriteMode();
-      Commands.sendHomeCoordinates(map_config.get('send_home_coords')[0], map_config.get('send_home_coords')[1], map_config.get('send_home_altitude'));
-    }, 500);//SERGE: GET RID OF THIS TIMEOUT SHIT ONCE YOU fiGURE THIS SHIT OUT
-
   });
 
   data_relay.on('close', function (had_error) {
-    TelemetryData.headers = [];
+    TelemetryData.headers = []; //this is important. So that we can properly parse the headers next time
     StatusManager.setStatusCode('DISCONNECTED_DATA_RELAY', true);
   });
 
@@ -44,10 +170,11 @@ module.exports.init = function () {
   });
 
   data_relay.on('data', function (data) {
-    if (!data) { //dont do anything if we get blank data or anything thats not an object
+    if (!data) { //don't do anything if we get blank data or anything that's not an object
       Logger.error('Got a blank packet from the data relay station. Value: ' + data);
       return;
     }
+
     data = data.toString();
 
     TelemetryData.received.push({
@@ -57,23 +184,12 @@ module.exports.init = function () {
 
     // First transmission is header columns
     if (TelemetryData.headers.length === 0) {
-      TelemetryData.headers = data.split(",").map(function (str) {
-        return str.trim();
-      });
-      Logger.debug('Network ' + data_relay.name + ' Received headers: ' + data);
-      Logger.debug('Network ' + data_relay.name + ' Parsed headers: ' + JSON.stringify(TelemetryData.headers));
-      Logger.data(TelemetryData.headers, 'DATA_RELAY_HEADERS');
-      StatusManager.addStatus('Received headers from data_relay', 3, 3000);
+      this.parseHeaders(data);
     }
     else { //if its the non-header columns(actual data)
-      var split_data = data.split(",");
-      for (var i = 0; i < split_data.length; i++) {
-        TelemetryData.current_state[TelemetryData.headers[i]] = split_data[i].trim().toString().replace('(', '').replace(')', ''); //the replace is required because theres a chance of random brackets being in the values
-      }
-      TelemetryData.state_history.push(TelemetryData.current_state);
-      TelemetryData.emit('data_received', TelemetryData.current_state);
-      Logger.data(JSON.stringify(TelemetryData.current_state), 'DATA_RELAY_DATA');
-      StatusManager.setStatusCode('TIMEOUT_DATA_RELAY', false);
+      this.parseData(data);
     }
-  });
+  }.bind(this));
 };
+
+module.exports = DataRelay;
