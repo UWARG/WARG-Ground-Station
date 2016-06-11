daf9eee7d08483fdc936b915c9198be2ec70f1d2
Serj Babayan <serj.b.babayan@gmail.com>
2016-06-11 14:12:02 -0400

    Fixed console window

--- app/windows/console.js
+++ app/windows/console.js
@@ -1,9 +1,16 @@
-//our new-window.js file (the window entry point)
-var gui=require('nw.gui');
+var ConsoleView = require('../../app/views/ConsoleView')(Marionette);
+var WindowShortcuts = require('../../app/core/WindowShortcuts');
 
-var consoleView=require('./app/views/ConsoleView')(Marionette);
-var WindowShortcuts=require('./app/util/WindowShortcuts')(Mousetrap, gui);
+WindowShortcuts.init(Mousetrap);
 
-$(document).ready(function(){
-  $('body').append((new consoleView()).render().$el);
+var window_view = new ConsoleView();
+
+$(document).ready(function () {
+  $('body').append(window_view.render().$el);
 });
+
+//call the destroy method to get rid of any event listeners that we're no longer going to be using
+window.onbeforeunload = (e) => {
+  window_view.destroy();
+  e.returnValue = true;
+};
