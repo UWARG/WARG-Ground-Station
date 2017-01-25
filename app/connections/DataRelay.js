646c8b1f4899e7a8499c4a2c4586158e86a786d5
Stephen Cholvat <stephen.cholvat@gmail.com>
2017-01-24 21:08:49 -0500

    Found data-relay IP

--- app/connections/DataRelay.js
+++ app/connections/DataRelay.js
@@ -49,47 +49,118 @@ var parseData = function (data) {
  * @function init
  */
 DataRelay.init = function () {
+//remove all data_relay connections
   if(NetworkManager.getConnectionByName('data_relay')){
     NetworkManager.removeAllConnections('data_relay');
   }
+//If legacy mode, try to connect to default IP/port via TCP
 
-  var data_relay = NetworkManager.addConnection('data_relay', network_config.get('datarelay_host'), network_config.get('datarelay_port'));
-
-  data_relay.setTimeout(network_config.get('datarelay_timeout'));
-
-  data_relay.on('connect', function () {
-    TelemetryData.clearHeaders();
-    StatusManager.setStatusCode('CONNECTED_DATA_RELAY', true);
-  });
-
-  data_relay.on('close', function (had_error) {
-    StatusManager.setStatusCode('DISCONNECTED_DATA_RELAY', true);
-  });
-
-  data_relay.on('timeout', function () {
-    StatusManager.setStatusCode('TIMEOUT_DATA_RELAY', true);
-  });
-
-  data_relay.on('write', function (data) {
-    StatusManager.addStatus('Sent command to data_relay', 3, 2000);
-  });
-
-  data_relay.on('data', function (data) {
-    if (!data) { //don't do anything if we get blank data or anything that's not an object
-      Logger.error('Got a blank packet from the data relay station. Value: ' + data);
-      return;
-    }
-
-    data = data.toString();
-
-    // First transmission is header columns
-    if (TelemetryData.getHeaders().length === 0) {
-      parseHeaders(data);
-    }
-    else { //if its the non-header columns(actual data)
-      parseData(data);
-    }
-  }.bind(this));
+//Else, get data_relay IP from UDP request and connect
+connectUDP('localhost','1234');
+  
 };
+  
+  var connectTCP = function(host,port){
+    var data_relay = NetworkManager.addConnection('data_relay', host, port);
+
+    data_relay.setTimeout(network_config.get('datarelay_timeout'));
+
+    data_relay.on('connect', function () {
+      TelemetryData.clearHeaders();
+      StatusManager.setStatusCode('CONNECTED_DATA_RELAY', true);
+    });
+
+    data_relay.on('close', function (had_error) {
+      StatusManager.setStatusCode('DISCONNECTED_DATA_RELAY', true);
+    });
+
+    data_relay.on('timeout', function () {
+      StatusManager.setStatusCode('TIMEOUT_DATA_RELAY', true);
+    });
+
+    data_relay.on('write', function (data) {
+      StatusManager.addStatus('Sent command to data_relay', 3, 2000);
+    });
+
+    data_relay.on('data', function (data) {
+      if (!data) { //don't do anything if we get blank data or anything that's not an object
+        Logger.error('Got a blank packet from the data relay station. Value: ' + data);
+        return;
+      }
+
+      data = data.toString();
+
+      // First transmission is header columns
+      if (TelemetryData.getHeaders().length === 0) {
+        parseHeaders(data);
+      }
+      else { //if its the non-header columns(actual data)
+        parseData(data);
+      }
+    }.bind(this));
+  }
+
+  var connectUDP = function(host,port){
+    var localport = 12345
+    var udp_open = false;
+    const dgram = require('dgram');
+    const server = dgram.createSocket('udp4');
+
+    server.on('error', (err) => {
+      console.log(`server error:\n${err.stack}`);
+      server.close();
+    });
+
+    server.on('message', (msg, rinfo) => {
+      console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+      console.log(rinfo.address,toString());
+      connectTCP(rinfo.address.toString(),msg.toString());
+
+      server.close();
+    });
+
+    server.on('close', (msg, rinfo) => {
+      udp_open = false;
+    });
+
+    server.on('listening', () => {
+      udp_open = true;
+      var address = server.address();
+      console.log(`server listening ${address.address}:${address.port}`);
+
+      setTimeout(function(){
+          if(udp_open){
+            console.log('UDP connection timed out after 1 second');
+            server.close();
+          }
+        },1000);
+    });
+
+    server.bind(localport);
+
+    //send IP and port to data_relay UDP port
+    var message = new Buffer(getLocalIP()+':'+localport);
+
+    var client = dgram.createSocket('udp4');
+    client.send(message, 0, message.length, port, host, function(err, bytes) {
+        if (err) throw err;
+        console.log('UDP message sent to ' + host +':'+ port);
+        client.close();
+    });
+  }
+
+  var getLocalIP = function(){
+    var os = require('os');
+
+    var interfaces = os.networkInterfaces();
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                return address.address;
+            }
+        }
+    }
+  }
 
 module.exports = DataRelay;
